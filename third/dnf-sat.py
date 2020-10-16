25f2f4d43e5cc1eecb8e01a180d96a219ac4bf2b
fand <fand@itu.dk>
2020-10-16 12:53:12 +0200

    working example

--- third/dnf-sat.py
+++ third/dnf-sat.py
@@ -3,6 +3,9 @@ import sys
 import random
 
 def convertDNF(dnfInput):
+    print("----- Testing DNF Input -----")
+    print(dnfInput)
+    print()
     names = dict()
     conjunctions = []
     clean = dnfInput.replace("(", "").replace(")", "")
@@ -15,49 +18,55 @@ def convertDNF(dnfInput):
         conjunctions.append(literals)
     return (names, conjunctions)
 
-def findSatAssignment(combined, rndConj):
+def findSatAssignment(names, rndConj):
+    result = dict()
+    for k in names:
+        result[k] = names[k]
     for i in range(0, len(rndConj)):
-        combined[rndConj[i]] = 0 if rndConj[i][0] == "!" else 1
-    return combined
+        result[rndConj[i]] = 0 if rndConj[i][0] == "!" else 1
+    return result
 
-def isMinimalIndex(conjunctions, index, names, conj):
+def isMinimalIndex(satConj, conjunctions, index):
+    # fasOrg = findSatAssignment(names, conjunctions[index])
     if index == 0:
         return True
     else:
+        count = index
+        print(count)
         for i in range(0, index):
-            v = findSatAssignment(names, conjunctions[i])
-            n = dict()
-            for k in v:
-                if v[k] != -1:
-                    n[k] = v[k]
-            print(n)
+            fat = findSatAssignment(dict(), conjunctions[i])
+            for k in fat.keys():
+                if k not in satConj or satConj[k] != fat[k]:
+                    # print(f"failed {satConj} != {fat}")
+                    count -= 1
+                    break
+        return count > 0
 
 def satAssigment(dnf):
     names, conjunctions = dnf 
     rndIndex = random.randint(0, len(conjunctions)-1)
     rndConj = conjunctions[rndIndex]
-    satAssigConj = findSatAssignment(names, rndConj)
+    satConj = findSatAssignment(names, rndConj)
     satAssig = dict()
 
-    isMinimal = isMinimalIndex(conjunctions, rndIndex, names, rndConj)
+    isMinimal = isMinimalIndex(satConj, conjunctions, rndIndex)
 
-    for k in satAssigConj.keys():
-        if satAssigConj[k] == -1:
+    for k in satConj.keys():
+        if satConj[k] == -1:
             satAssig[k] = random.randint(0,1)
         else:
-            satAssig[k] = satAssigConj[k]
+            satAssig[k] = satConj[k]
 
     for i in range(0,len(conjunctions)):
         for j in range(0,len(conjunctions[i])):
             v = conjunctions[i][j]
             conjunctions[i][j] = satAssig[v]
     
+    print(f"Satisfying index / conjunctions: {rndIndex} / {satConj}")
     print(f"Satisfying variables assignment: {satAssig}")
     print(f"Satisfying assignment: {conjunctions}")
     print(f"Is minimal index: {isMinimal}")
-
-    return satAssig
-
+    print()
 
 # Notations:
 # not: !
@@ -65,4 +74,11 @@ def satAssigment(dnf):
 # or: or
 
 dnf = convertDNF("(x1 and !x2 and !x3) or (x2 and x3) or (x2 and x4) or x7")
-a = satAssigment(dnf)
+satAssigment(dnf)
+
+
+dnf = convertDNF("(x1 and x2) or x1 or x2")
+satAssigment(dnf)
+
+dnf = convertDNF("(x1 and x2) or (x2 and x3) or (x1 and x3")
+satAssigment(dnf)
