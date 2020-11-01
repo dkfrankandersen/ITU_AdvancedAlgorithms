62dabb42896eadff1f67a876bfa2108a840e68d0
fand <fand@itu.dk>
2020-11-01 17:58:05 +0100

    some code

---
+++ fourth/LpTSP.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+
+class Edge(v, u):
+    f = v
+    t = u
+    def int
+
+
+    
+    
+class Graph:
+    vertices = []
+    edges = []
---
+++ fourth/MetricTSP.py
@@ -0,0 +1,50 @@
+import sys
+import math
+
+def readInput():
+    cities = []
+    for line in sys.stdin.readlines():
+        name, coords = line.split(",")
+        lat, lon = coords.split("/")
+        cities.append((name, float(lat), float(lon)))
+    return cities
+
+def buildGraph(cities):
+    graph = []
+    for c1 in cities:
+        for c2 in cities:
+            if not c1==c2:
+                _,lon1,lat1 = c1
+                _,lon2,lat2 = c2
+                weight = haversineDistance((lon1, lat1), (lon2, lat2))
+                graph.append((c1,c2, weight))
+    return graph
+
+def haversineDistance(u:float, v:float):
+    lat1, lon1 = u
+    lat2, lon2  = v
+    pi180 = math.pi/180
+    phi1 = lat1 * pi180
+    phi2 = lat2 * pi180
+    dPhi = (lat2-lat1) * pi180
+    dLambda = (lon1-lon2) * pi180
+
+    R = 6371e3 # metres
+    a = math.sin(dPhi/2)**2 + math.cos(phi1) * math.cos(phi2) * math.sin(dLambda/2)**2 # Haversine
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt((1-a)))
+    d = R*c
+    # print(d)
+    return d
+
+def computeMST(G):
+    return None
+
+def main():
+    cities = readInput()
+    graph = buildGraph(cities)
+    # print(cities)
+    print(graph)
+    print("HEJ HEK")
+    d = haversineDistance((50.0359, 5.4253), (58.3838, 3.0412))
+
+main()
