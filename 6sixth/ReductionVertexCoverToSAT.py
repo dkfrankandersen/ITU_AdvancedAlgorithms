8c1ab25ad55860de3e6fd8ffaade0598d58b0bc8
fand <fand@itu.dk>
2020-12-20 20:44:32 +0100

    Code for sheet6

---
+++ 6sixth/ReductionVertexCoverToSAT.py
@@ -0,0 +1,18 @@
+import sys
+import math
+import itertools
+
+
+
+
+
+def makeCycleOf24():
+    n = 24
+
+    for i in range(n):
+        for j in range(n):
+
+
+
+def reduceVertexCover(g, k):
+    
