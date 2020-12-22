0ff54be7c7f4ab1f6d74c12eb06bf9c001910c22
fand <fand@itu.dk>
2020-12-22 13:00:21 +0100

    sheet8 wip

--- 8eight/GridToNicePathDecomposition.py
+++ 8eight/GridToNicePathDecomposition.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 def gridToGraph(n,m):
     # Created vertice grid of n x m size
@@ -23,10 +24,42 @@ def gridToGraph(n,m):
 
     return vertices, sorted(edges)
 
+def adjMatrix(vertices, edges):
+    # Create adjacency matrix
+    N = len(vertices)
+    adj = np.array([[0 for _ in range(0, N)] for _ in range(0, N)])
+    for u,v in edges:
+        adj[u][v] = 1
+        adj[v][u] = 1
+    return adj
+
+
+class Node:
+    
+    def __init__(self, type):
+        self.type = type
+        self.bag = set()
+        self.child = None
+        self.type = None
+
+
+def decomposeGridGraph(a):
+    aT = a.T
+    T = []
+    for i in range(len(aT)):
+        t = []
+        if i > 0:
+            t.extend([v for v in aT[i-1]])
+        t.extend([v for v in aT[i]])
+        T.append(t)
+    return T
 
 if __name__ == "__main__":
-    V,E = gridToGraph(2,2)
-
-    print(V)
-    print(E)
+    vertices, edges = gridToGraph(2,2)
+    adj = adjMatrix(vertices, edges)
+    a =np.array([[11,12,13],[21,22,23],[31,32,33],[41,42,43]])
+    print(a)
+    print()
+    # print(np.array(adj))
+    decomposeGridGraph(a)
     pass
